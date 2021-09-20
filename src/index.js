bd74e0ba2287d927b28e33644736470cf855c9f3
tanulka <tanulkako@gmail.com>
2021-09-20 08:33:42 +0300

    hw-12 complete

--- src/index.js
+++ src/index.js
@@ -4,11 +4,9 @@ import countryCard from './templates/countryCard.hbs';
 import getRefs from './js/refs.js';
 import fetchCountries from './js/fetchCountries';
 
-import { error } from '@pnotify/core';
+import { alert, error } from '@pnotify/core';
 import '@pnotify/core/dist/PNotify.css';
-
-// let mark = countryList([1, 2, 3]);
-// console.log(mark);
+import '@pnotify/core/dist/BrightTheme.css';
 
 const refs = getRefs();
 
@@ -31,7 +29,7 @@ function renderCountryCard(country) {
       text: 'To many matches found. Please enter a more specific query!',
     });
   } else if (country.length > 1) {
-    let markup = country.map(c => `<li> ${c.name} </li>`).join(' ');
+    let markup = country.map(c => `<li><h2> ${c.name} </h2></li>`).join(' ');
     refs.list.innerHTML = markup;
   } else {
     markup = countryCard(country);
@@ -39,6 +37,7 @@ function renderCountryCard(country) {
   }
 }
 
-function onFetchError() {
-  alert('Not found country!');
+function onFetchError(error) {
+  console.log(error);
+  alert({ text: 'The name is not entered correctly. Please enter country name' });
 }
